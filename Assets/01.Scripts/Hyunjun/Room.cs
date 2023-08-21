9acd83864cdde4ed19419749e946712a835f4e4e
HyunjunDev <101493216+HyunjunDev@users.noreply.github.com>
2023-08-21 15:36:34 +0900

    내가 해냈어

---
+++ Assets/01.Scripts/Hyunjun/Room.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Room : MonoBehaviour
+{
+    public GameObject virtualCam;
+
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.CompareTag("Player") && !collision.isTrigger)
+        {
+            virtualCam.SetActive(true);
+        }
+    }
+
+    public void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player") && !collision.isTrigger)
+        {
+            virtualCam.SetActive(false);
+        }
+    }
+}
