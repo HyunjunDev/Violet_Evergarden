10130a5d867ecfe481a776b2810d9fb545c61893
HyunjunDev <101493216+HyunjunDev@users.noreply.github.com>
2023-08-28 13:42:51 +0900

    루아 스크립트 작업중

---
+++ Assets/01.Scripts/Hyunjun/ChatBox/GameState.cs
@@ -0,0 +1,40 @@
+using MoonSharp.Interpreter;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public enum ObjectType
+{
+    NONE,
+    PLAYER,
+    ENEMY
+}
+
+[MoonSharpUserData]
+public class GameState
+{
+    private ObjectType _selectedObj;
+
+    public ObjectType SelectedObj
+    {
+        get { return _selectedObj; }
+        [MoonSharpHidden]
+        set { _selectedObj = value; }
+    }
+
+    private Dictionary<ObjectType, GameObject> _objs;
+
+    public Dictionary<ObjectType, GameObject> Objs
+    {
+        get { return _objs; }
+        [MoonSharpHidden]
+        set { _objs = value; }
+    }
+
+    [MoonSharpHidden]
+    public GameState()
+    {
+        Objs=new Dictionary<ObjectType, GameObject>();
+    }
+}
