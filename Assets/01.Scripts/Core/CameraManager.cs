212ea0dc5f03c71698bea5225de90fd4c31e4770
myidmm64 <72392141+myidmm64@users.noreply.github.com>
2023-09-04 14:21:14 +0900

    0904_카메라 현재 vcam으로 설정

--- Assets/01.Scripts/Core/CameraManager.cs
+++ Assets/01.Scripts/Core/CameraManager.cs
@@ -6,21 +6,7 @@ using UnityEngine;
 
 public class CameraManager : MonoSingleTon<CameraManager>
 {
-    [SerializeField]
-    private CinemachineVirtualCamera _vCam = null;
-    private CinemachineBasicMultiChannelPerlin _noise = null;
-
     private Sequence _seq = null;
-
-    private void Awake()
-    {
-        if (_vCam == null)
-        {
-            Debug.LogError("VCam ¾øÀ½.");
-            return;
-        }
-        _noise = _vCam.GetCinemachineComponent<CinemachineBasicMultiChannelPerlin>();
-    }
 
     public void ResetCamera()
     {
@@ -29,13 +15,15 @@ public class CameraManager : MonoSingleTon<CameraManager>
 
     public void ShakeCamera(ShakeCameraDataSO data)
     {
+        CinemachineVirtualCamera vCam = CinemachineCore.Instance.GetActiveBrain(0).ActiveVirtualCamera.VirtualCameraGameObject.GetComponent<CinemachineVirtualCamera>();
+        CinemachineBasicMultiChannelPerlin noise = vCam.GetCinemachineComponent<CinemachineBasicMultiChannelPerlin>();
         _seq?.Kill();
-        _noise.m_AmplitudeGain = data.amplitude;
-        _noise.m_FrequencyGain = data.frequency;
+        noise.m_AmplitudeGain = data.amplitude;
+        noise.m_FrequencyGain = data.frequency;
         _seq = DOTween.Sequence();
         _seq.Append(DOTween.To(() => data.amplitude,
-            x => _noise.m_AmplitudeGain = x, 0f, data.shakeTime)).SetEase(data.easeType);
+            x => noise.m_AmplitudeGain = x, 0f, data.shakeTime)).SetEase(data.easeType);
         _seq.Join(DOTween.To(() => data.frequency,
-            x => _noise.m_FrequencyGain = x, 0f, data.shakeTime)).SetEase(data.easeType);
+            x => noise.m_FrequencyGain = x, 0f, data.shakeTime)).SetEase(data.easeType);
     }
 }
